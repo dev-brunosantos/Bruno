ff761b2730ef341322870675d9cf745f47dcc943
dev-brunosantos <100520104+dev-brunosantos@users.noreply.github.com>
2022-08-01 18:03:39 -0300

    Atualização

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const barraMenu = document.querySelector('.barra-de-menu');
+var corpo = document.querySelector('.corpo');
+var menu = document.querySelector('.cabecalho__menu');
 
+// FUNÇÃO PARA ABRIR/FECHAR MENU PRINCIPAL
 barraMenu.addEventListener('click', menuPrincipal);
 
 function menuPrincipal() {
 
-    var menu = document.querySelector('.cabecalho__menu');
     menu.classList.toggle('abrir-menu');
     
 }
+
+// FUNCÇÃO PARA FECHAR MENU
+corpo.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu');
+})
