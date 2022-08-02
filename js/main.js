36e8bbb508b03efc18317d61fbddfd6356d2e47a
dev-brunosantos <100520104+dev-brunosantos@users.noreply.github.com>
2022-08-02 15:48:39 -0300

    Novas Ações

--- js/main.js
+++ js/main.js
@@ -15,3 +15,42 @@ function menuPrincipal() {
 corpo.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu');
 })
+
+var tecnologias = document.querySelectorAll('.tecnologias');
+var htmlTech = document.getElementById('html');
+var cssTech = document.getElementById('css');
+var jsTech = document.getElementById('js');
+
+function html() {
+    animar();
+    htmlTech.style.display = "block";
+    cssTech.style.display = "none";
+    jsTech.style.display = "none";
+}
+function css() {
+    animar();
+    htmlTech.style.display = "none";
+    cssTech.style.display = "block";
+    jsTech.style.display = "none";
+}
+function js() {
+    animar();
+    htmlTech.style.display = "none";
+    cssTech.style.display = "none";
+    jsTech.style.display = "block";
+}
+
+function animar() {
+    for(i=0; i<tecnologias.length; i++) {
+        
+        htmlTech.classList.add('aparecer');
+        cssTech.classList.add('aparecer');
+        jsTech.classList.add('aparecer');
+        
+        setTimeout(function() {
+            htmlTech.classList.remove('aparecer');
+            cssTech.classList.add('aparecer');
+            jsTech.classList.add('aparecer');
+        },500)
+    }
+}
