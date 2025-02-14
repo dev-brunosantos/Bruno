a15fddcedc896825921124fb385397321868903c
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-14 11:38:02 -0300

    Alterando o temanaho da tela da sessão de projetos

--- src/app/_components/Projetos.tsx
+++ src/app/_components/Projetos.tsx
@@ -1,6 +1,6 @@
 export function Projetos() {
     return(
-        <section className="w-full h-full flex items-center justify-center">
+        <section className="w-full h-[100vh] flex items-center justify-center">
             <h1>Projetos</h1>
         </section>
     )
