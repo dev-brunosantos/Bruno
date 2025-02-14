4aa9e7062212586020b051ff3fc8a213f34dc192
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-14 10:46:04 -0300

    Selecionando os icones de tecnologias que tenho conhecimento

--- src/app/_components/Habilidades.tsx
+++ src/app/_components/Habilidades.tsx
@@ -1,7 +1,16 @@
+import { FaHtml5, FaCss3, FaJsSquare, FaReact } from 'react-icons/fa'
+import { RiNextjsFill, RiTailwindCssFill,RiBootstrapFill,RiFirebaseFill,RiNodejsLine } from 'react-icons/ri'
+import { DiMysql, DiPostgresql, DiSqllite, DiMongodb,DiSass   } from 'react-icons/di'
+import { BiLogoTypescript } from 'react-icons/bi'
+import { SiApachenifi, SiOracle, SiNestjs  } from 'react-icons/si'
+import { CardTech } from '@/src/components/Card/Tech'
+
 export function Habilidades() {
     return(
         <section>
             <h1>Habilidades</h1>
+
+            
         </section>
     )
 }
