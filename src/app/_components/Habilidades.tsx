37aff6f9d9b5b3e50d4d4e4e69484e1341442a3f
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-14 11:11:30 -0300

    Organizando as tecnologias

--- src/app/_components/Habilidades.tsx
+++ src/app/_components/Habilidades.tsx
@@ -4,15 +4,8 @@ import { DiMysql, DiPostgresql, DiSqllite, DiMongodb, DiSass } from 'react-icons
 import { BiLogoTypescript } from 'react-icons/bi'
 import { SiApachenifi, SiOracle, SiNestjs } from 'react-icons/si'
 import { CardTech } from '@/src/components/Card/Tech'
-import { IconType } from 'react-icons/lib'
 
 const icones = [
-    {
-        icone: RiNextjsFill
-    },
-    {
-        icone: DiMongodb
-    },
     {
         icone: FaHtml5
     },
@@ -20,25 +13,34 @@ const icones = [
         icone: FaCss3
     },
     {
-        icone: FaJsSquare
+        icone: RiBootstrapFill
     },
     {
-        icone: FaReact
+        icone: DiSass
     },
     {
         icone: RiTailwindCssFill
     },
     {
-        icone: RiBootstrapFill
+        icone: FaJsSquare
     },
     {
-        icone: RiFirebaseFill
+        icone: BiLogoTypescript
     },
     {
-        icone: RiNodejsLine
+        icone: FaReact
+    },
+    {
+        icone: RiNextjsFill
+    },
+    {
+        icone: DiMongodb
     },
     {
         icone: DiMysql
+    },
+    {
+        icone: SiOracle
     },
     {
         icone: DiPostgresql
@@ -47,21 +49,16 @@ const icones = [
         icone: DiSqllite
     },
     {
-        icone: BiLogoTypescript
-    },
-    {
-        icone: SiApachenifi
-    },
-    {
-        icone: SiOracle
+        icone: RiFirebaseFill
     },
     {
         icone: SiNestjs
     },
     {
-        icone: DiSass
+        icone: SiApachenifi
     }
 ]
+
 
 
 export function Habilidades() {
@@ -70,34 +67,6 @@ export function Habilidades() {
             <h1>Habilidades</h1>
 
             <div className='w-[100%] h-[100px] border-2 px-4 overflow-x-auto flex items-center gap-4'>
-                <CardTech>
-                    <FaHtml5 size={50} />
-                </CardTech>
-
-                <CardTech>
-                    <FaCss3 size={40} />
-                </CardTech>
-
-                <CardTech>
-                    <DiSass size={40} />
-                </CardTech>
-
-                <CardTech>
-                    <RiBootstrapFill size={40} />
-                </CardTech>
-
-                <CardTech>
-                    <RiTailwindCssFill size={40} />
-                </CardTech>
-
-                <CardTech>
-                    <FaJsSquare size={50} />
-                </CardTech>
-
-                <CardTech>
-                    <FaReact size={40} />
-                </CardTech>
-
                 {icones.map((card) => (
                     <CardTech>
                         <div>
